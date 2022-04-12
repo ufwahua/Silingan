524bf914cdba6731973bd963574985ed2f3ced88
Joshua Henry G. Diamos <jh.diamos@gmail.com>
2022-04-13 02:32:14 +0800

    feat: added announcement UI

--- resources/js/components/AnnouncementComponent.vue
+++ resources/js/components/AnnouncementComponent.vue
@@ -4,19 +4,31 @@
             class="col-12 sm:col-12 md:col-10 md:col-offset-1 lg:col-6 lg:col-offset-1 xl:col-6 xl:col-offset-1"
         >
             <div class="col justify-content-center pt-0">
-                <Fieldset class="mb-3" legend="Announcement">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                    </p>
-                </Fieldset>
+                <h1 class="text-center">Announcement</h1>
+                <div v-if="announcements">
+                    <div
+                        v-for="announcement in announcements"
+                        :key="announcement.id"
+                        class="card"
+                    >
+                        <span class="mb-5">
+                            <h4>
+                                Title:
+                                {{ announcement.title }}
+                            </h4>
+                            -{{ announcement.user.first_name }}
+                            {{ announcement.user.last_name }} [{{
+                                announcement.user.role
+                            }}]
+                        </span>
+                        <span><b>Content:</b> {{ announcement.content }}</span>
+                        <p class="text-right mt-5">
+                            {{ announcement.created_at }}
+                        </p>
+                    </div>
+                </div>
+
+                <div v-else class="text-center">No announcement</div>
             </div>
         </div>
         <div
@@ -108,6 +120,9 @@ export default {
         return {
             posts: computed(() => store.state.posts.posts),
             userLogged: computed(() => store.state.userLogged),
+            announcements: computed(
+                () => store.state.announcements.announcements
+            ),
         };
     },
     data() {
