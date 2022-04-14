3f62bc380fcbead6f7fc2f296f4f5096adc52fe7
varbs <varbiesumido@gmail.com>
2022-04-14 23:08:26 +0800

    Added Running Balance Column

--- database/migrations/2022_04_13_124359_create_collections_table.php
+++ database/migrations/2022_04_13_124359_create_collections_table.php
@@ -18,9 +18,9 @@ class CreateCollectionsTable extends Migration
             $table->foreignId('user_id');
             $table->foreignId('collection_type_id');
             $table->foreignId('block_lot_id');
-            $table->date('next_payment_date')->nullable();
             $table->double('amount', 8, 2);
-            $table->text('notes');
+            $table->text('notes')->nullable();
+            $table->double('running_balance',8,2);
             $table->timestamps();
         });
     }
